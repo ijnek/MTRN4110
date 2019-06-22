307b65407b2f23472b2b76e2c78c6d97c0c88fe2
Daniel Latimer <z5115175@student.unsw.edu.au>
2019-06-22 15:47:10 +1000

    Added some stuff to digital_pin

--- PhaseA/hardware.cpp
+++ PhaseA/hardware.cpp
@@ -43,20 +43,32 @@ auto hardware::digital_pin<pin>::low() -> void {
 template <hardware::pin_t pin>
 auto hardware::digital_pin<pin>::pwm_write(units::percentage duty_cycle) -> void{
     uint8_t pin_number_int = static_cast<uint8_t>(pin_number);
-
-    // TODO
-    //analogWrite(pin_number_int, ); // void analogWrite(uint8_t pin, int val);
+    int duty_cycle_int = static_cast<int>(duty_cycle.count());
+    int val = duty_cycle_int*255/100; // Map the duty cycle ([0, 100]) to an int output ([0, 255])
+                                      // Using the floor of this value should be good enough
+    analogWrite(pin_number_int, val); // void analogWrite(uint8_t pin, int val);
 }
 
 template <hardware::pin_t pin>
 auto hardware::digital_pin<pin>::pulse_length (logic_level state = logic_level::high,
     units::microseconds timeout = 1000000_us) -> units::microseconds {
-    
-    // TODO
-    using namespace units::literals;
-    auto one = 1_us;
-    return one;
+    unsigned long start = millis();
+    unsigned long end = millis();
+    unsigned long timeout_long = static_cast<unsigned long>(timeout.count());
+
+    while(end - start < timeout_long){ // While there is not a timeout, stay in the loop
+        // Check if the pin has changed it's state
+        // If it has, return the pulse length in microseconds
+
+        end = millis();
+    }
+
+    // Pulse length is > timeout, return timeout length
+    return timeout;
 }
 
-// External instantiation for class templates
+
+
+
+// External instantiation for class based on digital_pin
 template class hardware::digital_pin<2U>;
